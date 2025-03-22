f743cee25c7d0022dc58982bd6553554809fd098
yousef <yousef.ahmed7@gmail.com>
2025-03-22 14:42:47 +0200

    search page commit

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,26 +1,23 @@
 import { format } from "date-fns";
-import { motion } from "framer-motion";
 import Footer from "../components/Footer";
 import Header from "../components/header/Header";
 import { get_SearchResult } from "@/app/utils/api";
 import { SearchResultData } from "@/app/types/app";
 import ListingCard from "@/app/components/ListingCard";
 
-type CustomSearchParams = {
+type SearchParams = {
   location: string;
   startDate: string;
   endDate: string;
   numOfGuests: string;
 };
-
 const SearchResult = async ({
   searchParams: { location, startDate, endDate, numOfGuests },
 }: {
-  searchParams: CustomSearchParams;
+  searchParams: SearchParams;
 }) => {
-  let formatedStartDate = "";
-  let formatedEndDate = "";
-
+  let formatedStartDate;
+  let formatedEndDate;
   if (startDate && endDate) {
     formatedStartDate = format(new Date(startDate), "dd MMMM yy");
     formatedEndDate = format(new Date(endDate), "dd MMMM yy");
@@ -45,32 +42,25 @@ const SearchResult = async ({
           <p className="text-xs">
             300+ Stays - {range} - for {numOfGuests} guests
           </p>
-          <h1 className="text-3xl font-semibold mt-2 mb-6">
+          <h1 className="text-3xl font-semibold mt-2 mb-6 ">
             Stays in {location}
           </h1>
 
-          {/* Animated Filter Buttons */}
           <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
-            {filters.map((filter, index) => (
-              <motion.button
+            {filters.map((filter) => (
+              <button
                 type="button"
+                className="filter-btn px-4 py-2 border rounded-full transition-all duration-300 ease-in-out 
+                 hover:bg-gray-100 hover:scale-105 hover:shadow-md 
+                 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50 
+                 active:bg-gray-200 active:scale-95"
                 key={filter}
-                className="px-4 py-2 bg-gray-200 rounded-full text-sm font-medium transition-all duration-300 ease-in-out 
-                           hover:bg-gray-100 hover:scale-105 hover:shadow-md 
-                           focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50 
-                           active:bg-gray-200 active:scale-95"
-                initial={{ opacity: 0, y: 10 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.3, delay: index * 0.1 }}
-                whileHover={{ scale: 1.1 }}
-                whileTap={{ scale: 0.95 }}
               >
                 {filter}
-              </motion.button>
+              </button>
             ))}
           </div>
 
-          {/* Listings */}
           <div>
             {searchResultData.map((listing) => (
               <ListingCard
